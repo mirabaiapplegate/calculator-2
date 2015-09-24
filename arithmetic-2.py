cbd7888dad994e817be252c37f464c6606613259
Hackbright Student <info@hackbrightacademy.com>
2015-09-24 13:03:47 -0700

    Added any number inputs to add

---
+++ arithmetic-2.py
@@ -0,0 +1,37 @@
+def add(num1, *args):
+   total = num1
+   for arg in args:
+        total += arg
+
+   return total 
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    # Need to turn at least argument to float for division to
+    # not be integer division
+    return float(num1) / float(num2) 
+
+
+def square(num1):
+    # Needs only one argument
+    return num1 * num1
+
+
+def cube(num1):
+    # Needs only one argument
+    return num1 * num1 * num1
+
+
+def power(num1, num2):
+    return num1 ** num2  # ** = exponent operator
+
+
+def mod(num1, num2):
+    return num1 % num2
